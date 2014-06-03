295a99e93a87a43e8c37e8184102d555db8d2d18
Bartosz Dziewoński <matma.rex@gmail.com>
2014-06-03 23:35:47 +0200

    Nostalgia.php cleanup
    * Updated documentation
    * Removed pointless $dir = __DIR__
    * Removed pointless defined( 'MEDIAWIKI' ) check
    
    Change-Id: Id42c41edd3cab72f6301a1c5d632aa9b4e1acef3

--- Nostalgia.php
+++ Nostalgia.php
@@ -1,26 +1,20 @@
 <?php
 /**
- * Extension to provide the ancient nostalgia skin, plus SkinLegacy support
+ * The ancient Nostalgia skin, plus SkinLegacy support.
  */
 
-if( !defined( 'MEDIAWIKI' ) ) {
-	die( -1 );
-}
-
 $wgExtensionCredits['skin'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'Nostalgia',
+	'path' => __FILE__,
+	'name' => 'Nostalgia',
 	'descriptionmsg' => 'nostalgia-desc',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:Nostalgia',
+	'url' => 'https://www.mediawiki.org/wiki/Skin:Nostalgia',
 );
 
-// Include stuff!
-$dir = __DIR__;
-$wgAutoloadClasses['LegacyTemplate'] = "$dir/SkinLegacy.php";
-$wgAutoloadClasses['SkinLegacy'] = "$dir/SkinLegacy.php";
-$wgAutoloadClasses['SkinNostalgia'] = "$dir/Nostalgia_body.php";
+$wgAutoloadClasses['LegacyTemplate'] = __DIR__ . "/SkinLegacy.php";
+$wgAutoloadClasses['SkinLegacy'] = __DIR__ . "/SkinLegacy.php";
+$wgAutoloadClasses['SkinNostalgia'] = __DIR__ . "/Nostalgia_body.php";
 $wgMessagesDirs['Nostalgia'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['Nostalgia'] = "$dir/Nostalgia.i18n.php";
+$wgExtensionMessagesFiles['Nostalgia'] = __DIR__ . "/Nostalgia.i18n.php";
 $wgValidSkinNames['nostalgia'] = 'Nostalgia';
 $wgResourceModules['ext.nostalgia'] = array(
 	'styles' => array(
