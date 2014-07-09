1020ffdf360f00b079c4271a44aebc8f1b800e49
Bartosz Dziewoński <matma.rex@gmail.com>
2014-07-09 17:07:11 +0200

    Remove everything except for short information in README and Nostalgia.php
    The repository has been moved to mediawiki/skins/Nostalgia.
    
    Bug: 61256
    Change-Id: Iecc4100ec7f11e471c088fc751fd193f72ed360b

--- Nostalgia.php
+++ Nostalgia.php
@@ -1,32 +1,7 @@
 <?php
-/**
- * Extension to provide the ancient nostalgia skin, plus SkinLegacy support
- */
-
-if( !defined( 'MEDIAWIKI' ) ) {
-	die( -1 );
-}
-
-$wgExtensionCredits['skin'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'Nostalgia',
-	'descriptionmsg' => 'nostalgia-desc',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:Nostalgia',
-);
-
-// Include stuff!
-$dir = __DIR__;
-$wgAutoloadClasses['LegacyTemplate'] = "$dir/SkinLegacy.php";
-$wgAutoloadClasses['SkinLegacy'] = "$dir/SkinLegacy.php";
-$wgAutoloadClasses['SkinNostalgia'] = "$dir/Nostalgia_body.php";
-$wgMessagesDirs['Nostalgia'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['Nostalgia'] = "$dir/Nostalgia.i18n.php";
-$wgValidSkinNames['nostalgia'] = 'Nostalgia';
-$wgResourceModules['ext.nostalgia'] = array(
-	'styles' => array(
-		'screen.css',
-		'print.css' => array( 'media' => 'print' ),
-	),
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'Nostalgia',
-);
+// The Nostalgia extension has been converted to a standard skin.
+// If you see this file, you should remove the extension from your
+// MediaWiki installation and re-install the skin if you still intend
+// to use it.
+//
+// See: https://www.mediawiki.org/wiki/Skin:Nostalgia
